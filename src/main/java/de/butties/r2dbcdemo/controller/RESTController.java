6ffc2b67ea7e2c02444eaec9adc38cdc99c62db7
jonathan <jonathan@butties.de>
2023-11-11 21:04:40 +0100

    recursive non-repeating permutations

--- src/main/java/de/butties/r2dbcdemo/controller/RESTController.java
+++ src/main/java/de/butties/r2dbcdemo/controller/RESTController.java
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 
 import de.butties.r2dbcdemo.domain.Aggregate;
 import de.butties.r2dbcdemo.domain.Transaction;
@@ -28,6 +34,12 @@ public class RESTController {
 
     @GetMapping("/aggregates")
     public Flux<Aggregate> getAggregates() {
+
+        List<List<Integer>> result = subsets(new ArrayList<>(), Arrays.asList(0, 1, 2, 3, 5, 6, 4, 7), 3);
+        for(List<Integer> val : result) {
+            log.debug("GET getAggregates subsets {}", val);
+        }
+
         log.debug("GET getAggregates");
         return aggregateRepository.findAll();
     }
@@ -48,6 +60,24 @@ public class RESTController {
     public Mono<Transaction> processTransaction(@RequestBody Transaction transaction) {
         log.debug("POST processTransaction {}", transaction);
         return aggregateService.processTransaction(transaction);
+    }
+
+    static List<List<Integer>> subsets(List<Integer> root, List<Integer> in, int m) {
+        if (m < 1) {
+            return Collections.singletonList(root);
+        }
+        return in.stream()
+                .flatMap(e -> subsets(add(root, e), remove(in, e), m-1).stream()).collect(Collectors.toList());
+    }
+
+    static List<Integer> add(List<Integer> in, Integer e) {
+        List<Integer> out = new ArrayList<>(in);
+        out.add(e);
+        return out;
+    }
+
+    static List<Integer> remove(List<Integer> in, Integer e) {
+        return in.stream().filter(l -> l >= e).collect(Collectors.toList());
     }
 
 }
